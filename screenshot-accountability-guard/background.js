ffd97ad37be382dbd1dd77b48eba7a693f02f0e5
joshm21 <44173063+joshm21@users.noreply.github.com>
2022-03-16 00:34:25 -0500

    Update background.js

--- screenshot-accountability-guard/background.js
+++ screenshot-accountability-guard/background.js
@@ -1,18 +1,42 @@
-const onOtherExtensionDisabledOrUninstalled = (extensionInfo) => {
-  if (extensionInfo.name == "Screenshot Accountability") {
-    postToServer("disabledOrUninstalled", "Chromebook", null)
+const SERVER_ENDPOINT = ""
+
+
+
+const onOtherExtensionDisabledOrUninstalled = async (extensionInfo) => {
+  const thisName = await getExtensionName()
+  const otherName = extensionInfo.name
+  if (`${thisName} Guard` == otherName || thisName == `${otherName} Guard`) {
+    postToServer({ event: "partnerExtensionDisabledOrUninstalled" })
   }
 }
 
-const postToServer = (action, device, img) => {
-  console.log(`postToServer; action = ${action}`)
-  const url = "your_server_url_endpoint"
-  const data = { timestamp: new Date().toISOString(), action, device, img }
-  fetch(url, {
+const postToServer = async (payload) => {
+  payload.timestamp = new Date().toISOString()
+  payload.userEmail = await getUserEmail()
+  payload.source = await getExtensionName()
+  fetch(SERVER_ENDPOINT, {
     method: "POST",
-    body: JSON.stringify(data),
+    body: JSON.stringify({ payload }),
     headers: { "Content-Type": "text/plain;charset=utf-8" }
   })
 }
+
+
+
+
+const getUserEmail = async () => {
+  return new Promise(resolve => {
+    chrome.identity.getProfileUserInfo(profileUserInfo => resolve(profileUserInfo.email))
+  })
+}
+
+const getExtensionName = async () => {
+  return new Promise(resolve => {
+    chrome.management.getSelf(extensionInfo => resolve(extensionInfo.name))
+  })
+}
+
+
+
 
 chrome.management.onDisabled.addListener(onOtherExtensionDisabledOrUninstalled)
